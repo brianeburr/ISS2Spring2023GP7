d0b490f645936d28c774d4737c62f7cdca63d21d
Brian Burr <bburr@mines.edu>
2023-04-19 23:35:38 -0600

    Updated conditional symbol machine to tune based on training data

--- symbolMachine/SymbolMachineSuccessiveConditional.m
+++ symbolMachine/SymbolMachineSuccessiveConditional.m
@@ -1,32 +1,70 @@
 %Symbol machine use directions:
-
+%Training part, determine best value for initProbVal as proportion of
+%overall dataset size
 %1. initialize the symbol machine, setting global variables for overall
 %sequence, using below code. Set input file to parameter of init function
-sequenceLength = initializeSymbolMachine('sequences\sequence_DIAtemp_train.mat');
+
 
 %2. For this method, will be estimating the conditional probabilities for a
 %number based on the number previous. Data structure will be 2d array,
 %1 array containing probability distributions for a giving prior number
+sequenceName = 'DIAtemp';
+
+bestInitFactor = 0.1;
+bestPenalty = realmax; %initalize best penalty to max number
+
+for initFactor = 0:0.005:0.02
+    sequenceLength = initializeSymbolMachine(strcat('sequences\sequence_', sequenceName, '_train.mat'));
+    initProbVal = ceil(sequenceLength/9*bestInitFactor)+1; % initial value for probability distribution to be composed of, 
+    % assumes uniform distribution to start, prevents probability of zero,
+    % can be empirically tested to find best value
+    global SYMBOLDATA
+    sums = [[]];
+    for i = 1:9
+        for j = 1:9
+            sums(i,j) = initProbVal;
+        end
+    end
+    %3. For each element in sequence, run 'symbolMachine(pmf for given
+    %situation)', accepting return tuple of symbol,penalty
+    probs = sums(1,:)/sum(sums(1,:));
+    [symbol,penalty] = symbolMachine(probs); %must run once before loop because of different functionality
+    for ii = 2:sequenceLength
+        lastKnownSymbol = SYMBOLDATA.sequence(ii-1);  %finds most recent symbol to index into props matrix, give conditional probabilities
+        probs = sums(lastKnownSymbol,:)/sum(sums(lastKnownSymbol,:));
+        [symbol,penalty] = symbolMachine(probs);
+        sums(lastKnownSymbol, symbol) = sums(lastKnownSymbol, symbol) + 1;
+    end
+    %4. Run 'report symbol machine
+    %reportSymbolMachine;
+
+    if SYMBOLDATA.totalPenaltyInBits < bestPenalty
+        bestInitFactor = initFactor;
+        bestPenalty = SYMBOLDATA.totalPenaltyInBits;
+    end
+
+    disp("Testing initFactor: " + initFactor + ". Percent Guessed Correct: " + 100*SYMBOLDATA.correctPredictions/SYMBOLDATA.sequenceLength + ". Total Penalty: " + SYMBOLDATA.totalPenaltyInBits);
+end
+
+disp("Empirically Determined Best Init Factor: " + bestInitFactor)
 
 
-initProbVal = ceil(sequenceLength/9/2); % initial value for probability distribution to be composed of, 
-% assumes uniform distribution to start, prevents probability of zero,
-% can be empirically tested to find best value
 
+
+
+
+
+
+sequenceLength = initializeSymbolMachine(strcat('sequences\sequence_', sequenceName, '_test.mat'));
+initProbVal = ceil(sequenceLength/9*bestInitFactor)+1; % initial value for probability distribution to be composed of, 
 global SYMBOLDATA
-
 sums = [[]];
 for i = 1:9
     for j = 1:9
         sums(i,j) = initProbVal;
     end
 end
-
-%3. For each element in sequence, run 'symbolMachine(pmf for given
-%situation)', accepting return tuple of symbol,penalty
-
 probs = sums(1,:)/sum(sums(1,:));
-
 [symbol,penalty] = symbolMachine(probs); %must run once before loop because of different functionality
 for ii = 2:sequenceLength
     lastKnownSymbol = SYMBOLDATA.sequence(ii-1);  %finds most recent symbol to index into props matrix, give conditional probabilities
@@ -34,25 +72,8 @@ for ii = 2:sequenceLength
     [symbol,penalty] = symbolMachine(probs);
     sums(lastKnownSymbol, symbol) = sums(lastKnownSymbol, symbol) + 1;
 end
-
-%4. Run 'report symbol machine
 reportSymbolMachine;
 
-%{
-H = [[]]; %rows are first char, cols are second
-for i = 1:27
-    for j = 1:27
-        H(i,j) = 0;
-    end
-end
-
-for i = 1:length(A)-1
-   firstChar = A(i);
-   secondChar = A(i+1);
-   H(secondChar, firstChar) = H(secondChar, firstChar) + 1;
-end
-H = H/length(A);
-%}
 
 
 
